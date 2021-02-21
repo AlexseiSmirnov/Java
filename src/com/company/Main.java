ff64cb9d9eb44a82fc8e703e46f01df6a9fd72d4
AlexseySmirnov <alismirnov163@gmail.com>
2021-02-21 19:59:42 +0300

    New-000

---
+++ src/com/company/Main.java
@@ -0,0 +1,48 @@
+package com.company;
+import java.util.Scanner;
+
+public class Main {
+
+        public static void main(String[] args) {
+//        println выводит с новой строки and print выводит в одну строчку; \n перенос строки
+//        System.out.println("Hello World!");
+//        System.out.print("Hello\nHello");
+
+//        byte a = 8
+
+//        System.out.print("Введите число с точкой: ");
+//        Scanner num = new Scanner(System.in);
+//        System.out.println("Ваше число - " + num.nextFloat ());2.2
+
+//        int x = 15, a = 12;
+//        int res = x < a ? (x + a) : (x - a);
+//        System.out.println(res);
+//        int x = 10;
+//        switch (x){
+//            case 3:
+//                System.out.print("Три");
+//                break;
+//            case 7:
+//                System.out.print("Семь");
+//                break;
+//            default:
+//                System.out.print("Что-то другое");
+//                break;
+//
+//        for (int i = 10; i <= 20; i++)
+//            System.out.println(i * i);
+
+//        int a [] = new int[] {-14, 23, 9, -4, 12, -1};
+//        int max = 0;
+//        for (int i = 0; i < a.length; i++){
+//            if (a[i] > max)
+//                max = a[i];
+//        }
+//        System.out.println("Максимальный элемент: " + max);
+
+
+        }
+
+
+
+
